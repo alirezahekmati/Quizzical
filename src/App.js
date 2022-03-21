61558467de06fae9ff5e27bfdc0ee4b3c8de35c9
alireza <alirezahekmati80@gmail.com>
2022-03-21 07:39:44 -0700

    v1.0.3

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ useEffect(() => {
     setData(data.results)
   }
   getQuiz()
-}, [])
-
+}, [start])
+console.log("o")
 answerArray = data.map(e=> ([...e.incorrect_answers,e.correct_answer]))
 correctAnswerArray = data.map(e=>e.correct_answer)
 for(let i =0 ;i<data.length;i++){
@@ -42,9 +42,7 @@ for(let i =0 ;i<data.length;i++){
 function checkAnswer(){
   for(let i=0 ;i<correctAnswerArray.length;i++){
     finalGrade.push(correctAnswerArray[i] === answer[i]) 
-    console.log("yo1",answerJsx)
     setAnswerJsx(e=> [...e, <p className={`answer-p ${correctAnswerArray[i] === answer[i]}`}>{"your answer: "+answer[i]}  {"correct answer: "+correctAnswerArray[i]}</p>])
-    console.log("yo2",answerJsx) 
   }
   setEnd(true)
   setShowGrade(true)
@@ -57,13 +55,10 @@ function resetGame(){
   setGrade(0)
   setAnswer(["😶","😑","😢","😫","😭"])
 }
-console.log("yo3",answerJsx)
   return (
     <div className="App">
       <div className='circle Start-circle-1'></div>
       {showGrade ? answerJsx :(<div className="quiz-container">{start?<>{quizData}</> :  <Start startFunction={startGame}/>}</div>)}
-      
-       {/* {start? <Quiz question={data[0].question} answer={data[0].correct_answer} answers={answerArray[0]}/> :  <Start startFunction={startGame}/>} */}
       {start &&( end?( <><p>{"correct answer from 5 :" + Grade} </p><button onClick={resetGame}>start again</button></>) : (<button onClick={checkAnswer}>see the resuld </button>))}
       <div className='circle Start-circle-2'></div>
     </div>
